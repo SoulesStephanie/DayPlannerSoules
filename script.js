5b48d5958bbf929bd5ec6f352afcc995c1e71347
Violet Mae <rideuforfun@gmail.com>
2020-10-23 18:11:40 -0700

    finished if statements

--- script.js
+++ script.js
@@ -7,10 +7,85 @@ $(document).ready(function () {
     }
     
     $('#title').append(titleDate);
-    // shading logic...
+    // if statements
     if (moment().hour() === 9) {
         $('.plan0').css("background-color","red")
     }
     if (moment().hour() > 9) {
         $('.plan0').css("background-color","gray", "overflow", "hidden")
+    }
+    if (moment().hour() < 9) {
+        $('.plan0').css("background-color","green")
+    }
+    if (moment().hour() === 10) {
+        $('.plan1').css("background-color","red")
+    }
+    if (moment().hour() > 10) {
+        $('.plan1').css("background-color","gray")
+    }
+    if (moment().hour() < 10) {
+        $('.plan1').css("background-color","green")
+    }
+    if (moment().hour() === 11) {
+        $('.plan2').css("background-color","red")
+    }
+    if (moment().hour() > 11) {
+        $('.plan2').css("background-color","gray")
+    }
+    if (moment().hour() < 11) {
+        $('.plan2').css("background-color","green")
+    }
+    if (moment().hour() === 12) {
+        $('.plan3').css("background-color","red")
+    }
+    if (moment().hour() > 12) {
+        $('.plan3').css("background-color","gray")
+    }
+    if (moment().hour() < 12) {
+        $('.plan3').css("background-color","green")
+    }
+    if (moment().hour() === 13) {
+        $('.plan4').css("background-color","red")
+    }
+    if (moment().hour() > 13) {
+        $('.plan4').css("background-color","gray")
+    }
+    if (moment().hour() < 13) {
+        $('.plan4').css("background-color","green")
+    }
+    if (moment().hour() === 14) {
+        $('.plan5').css("background-color","red")
+    }
+    if (moment().hour() > 14) {
+        $('.plan5').css("background-color","gray")
+    }
+    if (moment().hour() < 14) {
+        $('.plan5').css("background-color","green")
+    }
+    if (moment().hour() === 15) {
+        $('.plan6').css("background-color","red")
+    }
+    if (moment().hour() > 15) {
+        $('.plan6').css("background-color","gray")
+    }
+    if (moment().hour() < 15) {
+        $('.plan6').css("background-color","green")
+    }
+    if (moment().hour() === 16) {
+        $('.plan7').css("background-color","red")
+    }
+    if (moment().hour() > 16) {
+        $('.plan7').css("background-color","gray")
+    }
+    if (moment().hour() < 16) {
+        $('.plan7').css("background-color","green")
+    }
+    if (moment().hour() === 17) {
+        $('.plan8').css("background-color","red")
+    }
+    if (moment().hour() > 17) {
+        $('.plan8').css("background-color","gray")
+    }
+    if (moment().hour() < 17) {
+        $('.plan8').css("background-color","green")
     }
